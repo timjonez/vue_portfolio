508ba4d52f6587cf0cbd1bf5cf5cf71aa3d6f361
tim <timjonez@protonmail.com>
2021-06-11 16:42:42 -0500

    [src/components] add header component and add title and nav items

---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1 class="text-white text-8xl text-center pt-16">Tim Jones</h1>
+      <ul class="m-auto justify-around mt-6 text-3xl">
+          <li><router-link to="/projects">Projects</router-link></li>
+          <li><router-link to="/resume">Resume</router-link></li>
+          <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+    color: #DBFF00;
+    display: flex;
+    width: 50%;
+}
+li {
+    padding: 10px 20px;
+}
+li:hover {
+    border-bottom: #DBFF00 2px solid;
+
+}
+</style>
