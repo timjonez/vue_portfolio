1b95cdf335790d8d7c38d42c4f7a25f4afafce1b
tim <timjonez@protonmail.com>
2021-06-14 12:01:13 -0500

    [src/views/Home] add about me section

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="home">
-    <h1>This is the home app</h1>
+  <div class="text-white h-2/3 grid justify-items-center">
+    <div class="pt-40" style="width: 800px">
+      <h1 class="text-7xl pb-4">About Me</h1>
+      <p class="leading-loose text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, sint adipisci. Excepturi mollitia maiores saepe dolores, aspernatur
+        aut pariatur, debitis, sit est
+        voluptatum facilis expedita? Aliquid est eius quas laudantium!
+      </p>
+    </div>
   </div>
 </template>
 
