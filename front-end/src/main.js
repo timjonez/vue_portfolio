7bff233b293e7eef3949d235a542dd6ed16021e0
tim <timjonez@protonmail.com>
2021-06-14 15:15:29 -0500

    [src/main.js] add fontawesome files

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue';
 import router from './router';
 import './assets/tailwind.css';
 import './assets/style.css';
+import './assets/fontawesome/css/all.css';
 
 createApp(App).use(router).mount('#app');
