b51f0c2eeb71a95e2cda471d15a319f3ec487165
tim <timjonez@protonmail.com>
2021-06-14 15:13:19 -0500

    [src/components/Footer] add a footer component

---
+++ front-end/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="footer m-auto flex justify-center py-5">
+        <a class="px-8" href="https://www.linkedin.com/in/tim-jones-424555193/" target="_blank">
+            <i class="fab fa-linkedin fa-3x"></i>
+        </a>
+        <a class="px-8" href="https://github.com/timjonez" target="_blank">
+            <i class="fab fa-github-square fa-3x"></i>
+        </a>
+    </footer>
+</template>
+
+<style  scoped>
+i {
+    color: #DBFF00 !important;
+}
+
+footer {
+    background: rgba(0, 0, 0, 0.65);
+    width: 100%;
+}
+</style>
