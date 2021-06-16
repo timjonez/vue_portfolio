521e455a5b5d1dc5f7a965c8e33634833c04579c
tim <timjonez@protonmail.com>
2021-06-16 16:42:40 -0500

    [scr/router/index] add routes for the new views

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,20 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import ProjectList from '../views/ProjectList.vue';
+import ProjectDetail from '../views/ProjectDetail.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/projects',
+    name: 'project-list',
+    component: ProjectList,
   },
+  {
+    path: '/project/:slug',
+    name: 'project-detail',
+    component: ProjectDetail,
+    props: true,
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
 ];
 
 const router = createRouter({
