5faee3508b7897dd1450d667e755d309e4a35434
tim <timjonez@protonmail.com>
2021-06-16 16:41:03 -0500

    [src/views/ProjectDetail] add a component for the project details

---
+++ front-end/src/views/ProjectDetail.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex-grow grid">
+        <h1 class="text-white-800">This is a project</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectDetail',
+  props: ['slug'],
+};
+</script>
