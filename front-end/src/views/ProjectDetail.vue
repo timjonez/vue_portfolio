961f0122872c073198c5f17c5cae8d0ad4d772fd
tim <timjonez@protonmail.com>
2021-06-29 20:49:19 -0500

    [src/components/projectdetail] add test data and start layout

--- front-end/src/views/ProjectDetail.vue
+++ front-end/src/views/ProjectDetail.vue
@@ -1,12 +1,66 @@
 <template>
-    <div class="flex-grow grid">
-        <h1 class="text-white-800">This is a project</h1>
+  <div id="content" class="flex-grow">
+    <div class="flex">
+      <div class="flex-1">
+        <h1 class="text-white title">{{ title }}</h1>
+        <h2 class="text-white text-xl">{{ subTitle }}</h2>
+      </div>
+      <div class="flex-1 flex pb-1 justify-end">
+        <a :href="liveLink"
+          target="_blank"
+          v-show="liveLink !== null"
+          class="rounded-full flex-initial text-center py-1.5 px-3 main-color">
+          View Live
+        </a>
+        <a :href="sourceLink"
+          target="_blank"
+          v-show="sourceLink !== null"
+          class="rounded-full flex-initial text-center py-1.5 px-3 main-color">
+          Source Code
+        </a>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ProjectDetail',
   props: ['slug'],
+  data() {
+    return {
+      key: 'trinity',
+      title: 'Trinity',
+      subTitle: 'Customer and inventory management system',
+      liveLink: null,
+      sourceLink: 'https://github.com/timjonez/volunteer_platform',
+      pictures: ['https://tim-jones.dev/media/findinghope.png'],
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nostrum dolores, nemo accusantium laboriosam tenetur ipsa nam esse, hic minus nobis? Ab a eaque aspernatur obcaecati dicta, itaque quaerat distinctio.',
+      techList: [
+        'Python',
+        'Django',
+        'Javascript',
+        'Jquery',
+        'HTMX',
+        'PostgreSQL',
+      ],
+    };
+  },
 };
 </script>
+
+<style scoped>
+#content {
+  width: 60%;
+  margin: auto;
+  margin-top: 5rem;
+}
+.title {
+  font-size: 2.5rem;
+}
+
+a {
+  height: fit-content;
+  margin-top: auto;
+}
+</style>
