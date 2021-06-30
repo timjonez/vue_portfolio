7b80eae0c9329b175a21213c831b83e8d2c128e9
tim <timjonez@protonmail.com>
2021-06-30 10:31:41 -0500

    [src/components/projectdetail] add image carousel

--- front-end/src/views/ProjectDetail.vue
+++ front-end/src/views/ProjectDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="content" class="flex-grow">
     <div class="flex">
-      <div class="flex-1">
+      <div class="flex-grow">
         <h1 class="text-white title">{{ title }}</h1>
         <h2 class="text-white text-xl">{{ subTitle }}</h2>
       </div>
@@ -20,13 +20,32 @@
         </a>
       </div>
     </div>
+    <Carousel :autoplay="5000" :wrap-around="true" :transition="800">
+      <Slide v-for="slide in pictures" :key="slide">
+        <div class="carousel__item">
+          <img :src="slide" height="200" class="carousel__item" alt="">
+        </div>
+      </Slide>
+
+      <template #addons>
+        <Pagination />
+      </template>
+    </Carousel>
   </div>
 </template>
 
 <script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Pagination, Slide } from 'vue3-carousel';
+
 export default {
   name: 'ProjectDetail',
   props: ['slug'],
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+  },
   data() {
     return {
       key: 'trinity',
@@ -34,7 +53,7 @@ export default {
       subTitle: 'Customer and inventory management system',
       liveLink: null,
       sourceLink: 'https://github.com/timjonez/volunteer_platform',
-      pictures: ['https://tim-jones.dev/media/findinghope.png'],
+      pictures: ['https://tim-jones.dev/media/findinghope.png', 'https://tim-jones.dev/media/findinghope.png'],
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nostrum dolores, nemo accusantium laboriosam tenetur ipsa nam esse, hic minus nobis? Ab a eaque aspernatur obcaecati dicta, itaque quaerat distinctio.',
       techList: [
         'Python',
