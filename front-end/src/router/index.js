fbbf396a25f3a93dc260ae2e6882115984949357
tim <timjonez@protonmail.com>
2021-07-01 17:02:45 -0500

    [src/router/index] add route for resume view

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import ProjectList from '../views/ProjectList.vue';
 import ProjectDetail from '../views/ProjectDetail.vue';
+import Resume from '../views/Resume.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: Resume,
   },
   {
     path: '/projects',
