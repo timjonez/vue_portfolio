b6e438b5db7b8c9b212a545d20bb91cbc1f4a5f9
tim <timjonez@protonmail.com>
2021-07-01 17:01:34 -0500

    [src/views/Resume] add view to hold resume

---
+++ front-end/src/views/Resume.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex-grow mx-auto pb-10">
+        <iframe src="../../TimJonesSoftwareEngineer.pdf"></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Resume',
+};
+</script>
+
+<style scoped>
+iframe {
+    width: 850px;
+    height: 1150px;
+}
+</style>
