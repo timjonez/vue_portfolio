f9d2c79144cdbc39be558fff84a9f529b799094c
tim <timjonez@protonmail.com>
2021-07-12 20:28:29 -0500

    [backend/service/db] update to correct db

--- backend/service/db.js
+++ backend/service/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/portfolioDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = {
   mongoose,
