75ae6a958c104ba665bf2ffb2a70a1b7cc83916f
tim <timjonez@protonmail.com>
2021-07-13 10:31:59 -0500

    [backend/service/db] add setting to stop deprecation warning

--- backend/service/db.js
+++ backend/service/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
 
 mongoose.connect("mongodb://localhost:27017/portfolioDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
