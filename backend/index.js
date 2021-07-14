c36c0ee84e2ac3a44429144d88f08900b2303964
tim <timjonez@protonmail.com>
2021-07-14 09:21:37 -0500

    [backned/index] add headers to client side access

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,13 @@ const api = require('./controllers/api')
 const app = express();
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
+
 app.route('/projects')
   .get(api.projects)
   .post(authenticateToken, api.addProject);
