14df42ad4d0c436ffb73c49d81953e61438b559b
tim <timjonez@protonmail.com>
2021-07-14 09:36:42 -0500

    [front-end/src/views/ProjectDetail] remove static data and add logic to fetch from api

--- front-end/src/views/ProjectDetail.vue
+++ front-end/src/views/ProjectDetail.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="content" class="flex-grow pb-10">
-    <div class="flex">
+    <div class="flex pb-1">
       <div class="flex-grow">
-        <h1 class="text-white title">{{ title }}</h1>
-        <h2 class="text-white text-xl">{{ subTitle }}</h2>
+        <h1 class="text-white title">{{ project.title }}</h1>
+        <h2 class="text-white text-xl">{{ project.subTitle }}</h2>
       </div>
       <div class="flex-1 flex pb-1 justify-end">
-        <a :href="liveLink"
+        <a :href="project.liveLink"
           target="_blank"
-          v-show="liveLink !== null"
+          v-show="project.liveLink !== null"
           class="rounded-full flex-initial text-center py-1.5 px-3 main-color">
           View Live
         </a>
-        <a :href="sourceLink"
+        <a :href="project.sourceLink"
           target="_blank"
-          v-show="sourceLink !== null"
-          class="rounded-full flex-initial text-center py-1.5 px-3 main-color">
+          v-show="project.sourceLink !== null"
+          class="rounded-full flex-initial text-center ml-2 py-1.5 px-3 main-color">
           Source Code
         </a>
       </div>
     </div>
     <Carousel :autoplay="5000" :wrap-around="true" :transition="800">
-      <Slide v-for="slide in pictures" :key="slide">
+      <Slide v-for="slide in project.pictures" :key="slide">
         <div class="carousel__item">
           <img :src="slide" height="200" class="carousel__item" alt="">
         </div>
@@ -34,12 +34,12 @@
     <div class="flex gap-10 my-10">
       <div class="flex-1">
         <h2 class="text-white text-3xl pb-3">About</h2>
-        <p class="text-white">{{ desc }}</p>
+        <p class="text-white">{{ project.desc }}</p>
       </div>
       <div class="flex-1">
-        <h2 class="text-white text-3xl pb-3">Technical Sheet</h2>
+        <h2 class="text-white text-3xl pb-3">Technologies</h2>
         <ul class="text-white">
-          <li v-for="item in techList" :key="item">{{ item }}</li>
+          <li v-for="item in project.techList" :key="item">{{ item }}</li>
         </ul>
       </div>
     </div>
@@ -49,6 +49,9 @@
 <script>
 import 'vue3-carousel/dist/carousel.css';
 import { Carousel, Pagination, Slide } from 'vue3-carousel';
+import axios from 'axios';
+
+const apiUrl = 'http://192.168.0.5:3000';
 
 export default {
   name: 'ProjectDetail',
@@ -60,22 +63,25 @@ export default {
   },
   data() {
     return {
-      key: 'trinity',
-      title: 'Trinity',
-      subTitle: 'Customer and inventory management system',
-      liveLink: null,
-      sourceLink: 'https://github.com/timjonez/volunteer_platform',
-      pictures: ['https://tim-jones.dev/media/findinghope.png', 'https://tim-jones.dev/media/findinghope.png'],
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nostrum dolores, nemo accusantium laboriosam tenetur ipsa nam esse, hic minus nobis? Ab a eaque aspernatur obcaecati dicta, itaque quaerat distinctio.',
-      techList: [
-        'Python',
-        'Django',
-        'Javascript',
-        'Jquery',
-        'HTMX',
-        'PostgreSQL',
-      ],
+      project: {
+        title: '',
+        subTitle: '',
+        liveLink: null,
+        sourceLink: null,
+        desc: '',
+        techList: [],
+        pictures: [],
+      },
     };
+  },
+  mounted() {
+    axios.get(`${apiUrl}/projects/${this.$route.params.slug}`)
+      .then((res) => {
+        [this.project] = res.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
