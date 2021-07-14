1deaedfb3b82622d5f63affe92cbc4845eaf6009
tim <timjonez@protonmail.com>
2021-07-14 09:25:17 -0500

    [front-end/src/main] add in sweet alert

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
 import './assets/tailwind.css';
 import './assets/style.css';
 import './assets/fontawesome/css/all.css';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(VueSweetalert2).mount('#app');
