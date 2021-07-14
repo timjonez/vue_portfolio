42365fc4cc54be2341729b8bac588ed31c75132f
tim <timjonez@protonmail.com>
2021-07-14 09:26:35 -0500

    [front-end/src/views/Contact] adjust the form, and add in the logic to handle submissions

--- front-end/src/views/Contact.vue
+++ front-end/src/views/Contact.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="flex-grow">
-        <form action="" method="post" class="flex flex-col w-1/2 mx-auto">
+        <form @submit.prevent="sendEmail" class="flex flex-col w-11/12 md:w-1/2 mx-auto">
             <label for="name">Name</label>
-            <input type="text" name="name">
+            <input type="text" name="name" v-model="name">
             <label for="email">Email</label>
-            <input type="email" name="email">
+            <input type="email" name="email" v-model="email">
+            <label for="subject">Subject</label>
+            <input type="text" name="subject" v-model="subject">
             <label for="message">Message</label>
-            <textarea name="message" cols="30" rows="10"></textarea>
+            <textarea name="message" cols="30" rows="10" v-model="message"></textarea>
             <button type="submit"
              class="main-color rounded-full mt-5 ml-auto py-1.5 font-bold text-xl px-3 w-1/4">
               Send
@@ -16,8 +18,45 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+const apiUrl = 'http://192.168.0.5:3000';
+
 export default {
   name: 'Contact',
+  data() {
+    return {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    };
+  },
+  methods: {
+    sendEmail() {
+      axios.post(`${apiUrl}/contact`, {
+        name: this.name,
+        email: this.email,
+        subject: this.subject,
+        message: this.message,
+      })
+        .then(() => {
+          this.$swal({
+            icon: 'success',
+            title: 'Success!',
+            text: 'Your message has successfully been sent\nYou will be redirected shortly',
+            showConfirmButton: false,
+            timer: 2500,
+          })
+            .then(() => {
+              window.location.href = '/';
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
