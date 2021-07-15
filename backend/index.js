9429eab4e336a1d8655d7330086dc8c680b6a3f8
tim <timjonez@protonmail.com>
2021-07-15 16:25:58 -0500

    [backend/index] set up static files

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use('/images', express.static('../uploads'))
+
 app.use(fileUpload({
   createParentPath: true
 }));
