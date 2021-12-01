152fbaf4c6cf533918ff5b9384562d7e23ce84ed
Tim Jones <timjonez@protonmail.com>
2021-11-30 20:06:33 -0600

    [front-end/src/views/Home] update the about section

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -6,8 +6,9 @@
         I am a self-taught software engineer, and a lifelong learner. As a natural problem-solver,
         I’ve always found it natural to seek solutions for problems I’ve encountered.
         In 2019 I started applying these problem solving skills in the software
-        engineering space, and never looked back.<br> I am currently a full-time freelancer,
-        and as such I focus on engineering solutions for my client’s problems.
+        engineering space, and never looked back.<br> I am currently a software engineer with
+        <a href="https://www.infinite.com/" target="_blank">Infinite Computer Solutions(ICS)</a>,
+        but I also do freelance projects on the side.
       </p>
     </div>
   </div>
