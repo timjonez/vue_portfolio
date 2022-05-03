bfb02bbc1900b7f43290d1b15dc43ebe24054e0f
tim <timjonez@protonmail.com>
2022-05-02 22:05:24 -0500

    [front-end/comps] update linked url and resume file

--- front-end/src/components/Footer.vue
+++ front-end/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer m-auto flex justify-center py-5">
-        <a class="px-8" href="https://www.linkedin.com/in/tim-jones-424555193/" target="_blank">
+        <a class="px-8" href="https://www.linkedin.com/in/tim-jonez/" target="_blank">
             <i class="fab fa-linkedin fa-3x"></i>
         </a>
         <a class="px-8" href="https://github.com/timjonez" target="_blank">
