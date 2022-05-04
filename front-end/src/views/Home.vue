653e646f0837dd6268d5f7ea9a3ac5479e1593c5
Tim Jones <timjonez@protonmail.com>
2022-05-04 10:28:27 -0500

    [frontend/comps/home] update bio

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -3,15 +3,12 @@
     <div class="pt-10 w-11/12  md:w-1/2">
       <h1 class="text-7xl pb-4">About Me</h1>
       <p class="leading-loose text-lg">Hi, and welcome to my portfolio.
-        I am a self-taught software engineer, and a lifelong learner. As a natural problem-solver,
-        I’ve always found it natural to seek solutions for problems I’ve encountered.
+        I am a self-taught software engineer and a lifelong learner. As a natural problem-solver,
+        I've always found it natural to seek solutions for problems I've encountered.
         In 2019 I started applying these problem solving skills in the software engineering space,
         and never looked back.<br>
-        I am currently a full stack software engineer at
-        <a href="https://bluewater-fintech.com/" target="_blank">
-          Blue Water Financial Technologies
-        </a>
-        where I enjoy creating solutions for the mortgage servicing industry.
+        I am currently looking for my next position, as well as doing some part-time freelancing
+        on the side.
       </p>
     </div>
   </div>
