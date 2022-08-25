cd931b3d1ba5ff8da37ef22a4805f2c9c3cd04ec
Tim Jones <timjonez@protonmail.com>
2022-08-25 16:45:12 -0500

    [rust/src/app] add function to get database connection

--- rust_backend/src/app.rs
+++ rust_backend/src/app.rs
@@ -1,5 +1,12 @@
 use actix_web::{App, HttpServer};
 use crate::routes;
+use sqlx::{SqlitePool};
+
+pub async fn get_db_connection() -> SqlitePool {
+    SqlitePool::connect("sqlite://db.sqlite3")
+        .await
+        .expect("Failed to connect to database")
+}
 
 pub async fn run() -> std::io::Result<()> {
     let server = HttpServer::new(move|| {
