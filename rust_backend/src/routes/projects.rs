d3cdd8272c5c698e286c2fceafadd0cf9ceed3aa
Tim Jones <timjonez@protonmail.com>
2022-08-25 16:43:29 -0500

    [rust/src/routes/projects] add route logic to return projects

--- rust_backend/src/routes/projects.rs
+++ rust_backend/src/routes/projects.rs
@@ -1,6 +1,13 @@
-use actix_web::get;
+use actix_web::{get, HttpResponse};
+use crate::app::get_db_connection;
+use crate::domain::Project;
 
 #[get("/projects")]
-pub async fn get_project_list() -> String {
-    "Hello World".to_owned()
+pub async fn get_project_list() -> HttpResponse {
+    let db_pool = get_db_connection().await;
+    let projects = sqlx::query_as!(Project, "select * from projects where active = 1")
+        .fetch_all(&db_pool)
+        .await
+        .expect("Failed to get projects");
+    HttpResponse::Ok().json(projects)
 }
