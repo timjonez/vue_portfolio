418a60f17183244b7b278566cee6f4fd008f89ee
Tim Jones <timjonez@protonmail.com>
2022-08-25 11:22:19 -0500

    [rust/src/main] try out project creation in main

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,4 +1,8 @@
+use rust_backend::domain::Project;
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+    let project = Project::new().expect("Failed to create project");
+    println!("project: {:?}", project)
 }
