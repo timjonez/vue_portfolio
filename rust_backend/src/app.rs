930721dc2d8640fa284e4d97086beee45cc41dd8
Tim Jones <timjonez@protonmail.com>
2022-08-26 17:00:49 -0500

    [rust/src/app] add another route and a custom error handler

--- rust_backend/src/app.rs
+++ rust_backend/src/app.rs
@@ -1,6 +1,9 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web, error, Error, HttpResponse};
 use crate::routes;
 use sqlx::{SqlitePool};
+use serde_json;
+use actix_web::error::JsonPayloadError::Deserialize;
+use crate::utils::JsonError;
 
 pub async fn get_db_connection() -> SqlitePool {
     SqlitePool::connect("sqlite://db.sqlite3")
@@ -12,6 +15,18 @@ pub async fn run() -> std::io::Result<()> {
     let server = HttpServer::new(move|| {
         App::new()
             .service(routes::get_project_list)
+            .service(routes::save_project)
+            .app_data(
+                web::JsonConfig::default().error_handler(|err, req| {
+                        match err {
+                            Deserialize(d) => {
+                                let e = JsonError::new(d.to_string());
+                                error::ErrorBadRequest(serde_json::to_string(&e).unwrap())
+                            },
+                            _ => Error::from(err)
+                        }
+                    })
+            )
     })
     .bind(("127.0.0.1", 8000))?
     .run();
