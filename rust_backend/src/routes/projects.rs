6ecce44a2f06c9212fe76153317eafc31c322f03
Tim Jones <timjonez@protonmail.com>
2022-08-26 17:01:47 -0500

    [rust/src/routes/projects] add route for posting new projects

--- rust_backend/src/routes/projects.rs
+++ rust_backend/src/routes/projects.rs
@@ -1,6 +1,7 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{get, post, web, HttpResponse};
 use crate::app::get_db_connection;
-use crate::domain::Project;
+use crate::domain::{Project, ProjectForm};
+use crate::utils::JsonError;
 
 #[get("/projects")]
 pub async fn get_project_list() -> HttpResponse {
@@ -10,4 +11,41 @@ pub async fn get_project_list() -> HttpResponse {
         .await
         .expect("Failed to get projects");
     HttpResponse::Ok().json(projects)
+}
+
+#[post("/project/")]
+pub async fn save_project(project_data: web::Json<ProjectForm>) -> HttpResponse {
+    let db_pool = get_db_connection().await;
+    let project_form: ProjectForm = project_data.0.into();
+    let project = match Project::try_from(project_form) {
+        Ok(p) => p,
+        Err(_) => return HttpResponse::BadRequest().json(JsonError::new("Failed to create project".to_string()))
+    };
+
+    let query = sqlx::query!(
+        r#"
+            INSERT INTO projects (id, slug, title, sub_title, live_link, source_link, description, active)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        "#,
+        project.id,
+        project.slug,
+        project.title,
+        project.sub_title,
+        project.live_link,
+        project.source_link,
+        project.description,
+        project.active,
+    )
+    .execute(&db_pool)
+    .await;
+
+    match query {
+        Ok(_) => HttpResponse::Created().json(project),
+        Err(e) => {
+            if e.to_string().contains("UNIQUE") {
+                return HttpResponse::BadRequest().json(JsonError::new("Project with this slug already exists".to_string()))
+            }
+            HttpResponse::BadRequest().json(JsonError::new(e.to_string()))
+        }
+    }
 }
