f9a80eb3d7618c3e15558ff118f3178ddbb9187a
Tim Jones <timjonez@protonmail.com>
2022-08-26 17:02:50 -0500

    [rust/src/domain/projects] add struct for project form

--- rust_backend/src/domain/project.rs
+++ rust_backend/src/domain/project.rs
@@ -27,5 +27,40 @@ impl Project {
         };
         Ok(project)
     }
-    
+    pub fn generate_slug(value: String) -> String {
+        value.to_lowercase().replace(" ", "-")
+    }
+}
+
+impl TryFrom<ProjectForm> for Project {
+    type Error = String;
+
+    fn try_from(value: ProjectForm) -> Result<Self, Self::Error> {
+        let slug = match value.slug {
+            Some(slug) => slug,
+            None => Self::generate_slug(value.title.clone())
+        };
+
+        Ok(Self {
+            id: Uuid::new_v4().to_string(),
+            slug,
+            title: value.title,
+            sub_title: value.sub_title,
+            live_link: value.live_link,
+            source_link: value.source_link,
+            description: value.description,
+            active: true,
+        })
+    }
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct ProjectForm {
+    pub slug: Option<String>,
+    pub title: String,
+    pub sub_title: Option<String>,
+    pub live_link: Option<String>,
+    pub source_link: Option<String>,
+    pub description: String,
 }
