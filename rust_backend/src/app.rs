f0bf7f5d7125f03cfc33832a94d66ab6c842f239
Tim Jones <timjonez@protonmail.com>
2022-08-29 15:07:20 -0500

    [rust/src/app] clean up imports and format

--- rust_backend/src/app.rs
+++ rust_backend/src/app.rs
@@ -1,9 +1,7 @@
-use actix_web::{App, HttpServer, web, error, Error, HttpResponse};
 use crate::routes;
-use sqlx::{SqlitePool};
-use serde_json;
-use actix_web::error::JsonPayloadError::Deserialize;
 use crate::utils::JsonError;
+use actix_web::{error, web, App, HttpResponse, HttpServer};
+use sqlx::SqlitePool;
 
 pub async fn get_db_connection() -> SqlitePool {
     SqlitePool::connect("sqlite://db.sqlite3")
@@ -12,22 +10,19 @@ pub async fn get_db_connection() -> SqlitePool {
 }
 
 pub async fn run() -> std::io::Result<()> {
-    let server = HttpServer::new(move|| {
+    let server = HttpServer::new(move || {
         App::new()
             .service(routes::get_project_list)
             .service(routes::save_project)
-            .app_data(
-                web::JsonConfig::default().error_handler(|err, req| {
-                        error::InternalError::from_response(
-                            "",
-                            HttpResponse::BadRequest()
-                                .content_type("application/json")
-                                .json(JsonError::new(err.to_string()))
-
-                        )
-                        .into()
-                    })
-            )
+            .app_data(web::JsonConfig::default().error_handler(|err, req| {
+                error::InternalError::from_response(
+                    "",
+                    HttpResponse::BadRequest()
+                        .content_type("application/json")
+                        .json(JsonError::new(err.to_string())),
+                )
+                .into()
+            }))
     })
     .bind(("127.0.0.1", 8000))?
     .run();
