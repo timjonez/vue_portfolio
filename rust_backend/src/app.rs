dd6cb1a2dd5c7e0f76b74a7a7f5354d50fc7dfb5
Tim Jones <timjonez@protonmail.com>
2022-08-29 16:32:45 -0500

    [rust/src/app] register route handler

--- rust_backend/src/app.rs
+++ rust_backend/src/app.rs
@@ -13,6 +13,7 @@ pub async fn run() -> std::io::Result<()> {
     let server = HttpServer::new(move || {
         App::new()
             .service(routes::get_project_list)
+            .service(routes::get_project)
             .service(routes::save_project)
             .app_data(web::JsonConfig::default().error_handler(|err, req| {
                 error::InternalError::from_response(
